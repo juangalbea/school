abb239ab80650e6422a7283b08d84838385a5fc5
unknown <juangalbea@gmail.com>
2018-11-16 20:54:20 +0000

    first commit

---
+++ school.js
@@ -0,0 +1,65 @@
+class School {
+  constructor(name, level, numberOfStudents) {
+    this._name = name;
+    this._level = level;
+    this._numberOfStudents = numberOfStudents;
+  }
+  get name() {
+    return this._name;
+  }
+  get level() {
+    return this._level;
+  }
+  get numberOfStudents() {
+    return this._numberOfStudents;
+  }
+  set numberOfStudents(value) {
+    if (value.isNaN()) {
+      console.log('Invalid input: numberOfStudents must set to a Number');
+    } else {
+      this._numberOfStudents = value;
+    }
+  }
+  quickFacts() {
+    console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`);
+  }
+  static pickSubstituteTeacher(substituteTeachers) {
+    let generated = Math.floor(Math.random() * (substituteTeachers.length -1));
+    return substituteTeachers[generated];
+  }
+}
+
+class PrimarySchool extends School{
+  constructor(name, numberOfStudents, pickupPolicy) {
+    super(name, 'primary', numberOfStudents);
+    this._pickupPolicy = pickupPolicy;
+  }
+  get pickupPolicy() {
+    return this._pickupPolicy;
+  }
+}
+
+class MiddleSchool extends School{
+  constructor(name, numberOfStudents) {
+    super(name);
+  }
+}
+
+class HighSchool extends School{
+  constructor(name, numberOfStudents, sportsTeams) {
+    super(name, 'High School', numberOfStudents);
+    this._sportsTeams = sportsTeams;
+  }
+  get sportsTeams() {
+    return this._sportsTeams;
+  }
+}
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+
+lorraineHansbury.quickFacts();
+const sub = School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+
+console.log(alSmith.sportsTeams);
